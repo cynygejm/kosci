d6e19f5f23cdccb3d18c2ef3dcc4f378f64f339a
cynygejm <asiaglina@o2.pl>
2017-10-21 23:35:08 +0200

    zrobiłam pierwszy przycisk OK

--- kosci.js
+++ kosci.js
@@ -1,1 +1,13 @@
-var startbutton=document.querySelector('');
+
+
+function sprawdz(){
+    var imie = document.getElementById("name").value;
+    if (imie.length <=0){
+        alert ("Wpisz imię");
+    }
+        else {
+            return imie;
+        }
+    }
+
+
