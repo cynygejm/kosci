d91a63d8b8d560fcd0f702e2ff4fc71b037d618f
cynygejm <asiaglina@o2.pl>
2017-10-22 13:42:13 +0200

    próba losowania

--- kosci.js
+++ kosci.js
@@ -1,5 +1,5 @@
 
-
+ /*var imie = document.getElementById("name").value;*/  /*----czy to nie powinno być tu, u góry?    */
 function sprawdz(){
     var imie = document.getElementById("name").value;
     if (imie.length <=0){
@@ -8,6 +8,19 @@ function sprawdz(){
         else {
             return imie;
         }
+    } 
+
+function losowanie (){
+    var imie = document.getElementById("name").value;  /* czy to trzeba przepisywać? */
+    var gracz = 0;
+    var wylosowany=1;
+    if (wylosowany) {
+        wylosowany = imie;
     }
+    else {
+        wylosowany = gracz;
+    }
+    return wylosowany;
+} 
 
 
