787e239690023e28e792ed738cd2c8f7b5a48a61
cynygejm <asiaglina@o2.pl>
2017-11-18 20:33:00 +0100

    new css

--- kosci2.js
+++ kosci2.js
@@ -1,6 +1,6 @@
 var addName= document.querySelector("#name");
 var okButton= document.querySelector(".okbutton");
-okButton.addEventListener("click", function sprawdz(){
+okButton.addEventListener("click", function check(){
     var name = document.querySelector("#name").value;
     if (name.length <=0){
           alert ("Wpisz imię");
@@ -8,20 +8,20 @@ okButton.addEventListener("click", function sprawdz(){
     else{console.log(name);}
     });
 var randomButton=document.querySelector("#random");
-randomButton.addEventListener("click", function random(){    
+randomButton.addEventListener("click", function random(){
     var name = document.querySelector("#name").value;
     var randomNr = Math.round(Math.random());
-    
+
     if (randomNr === 0){
         write.innerHTML= name;
-    } 
+    }
     else{
       write.innerHTML= "Janusz Gracz";
-    } 
+    }
 });
 
 var startButton=document.querySelector("#start");
 var container =document.querySelector(".container");
 startButton.addEventListener("click", function changeContainer (){
-  
+//    co dalej? jak zmienić "planszę?""
 })
